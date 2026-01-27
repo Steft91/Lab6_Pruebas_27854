137143ab8a3a44c6ea5fa6e6b8346c86c9f5953d
Steft91 <stefany152005@gmail.com>
2026-01-27 09:54:29 -0500

    Proyecto base con CI

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Integración Continua trabajando!');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor trabajando en el puerto ${port}`);
+});
