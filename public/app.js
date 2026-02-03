dfba541cad3b80601ceb7429460acd7849e42c60
Steft91 <stefany152005@gmail.com>
2026-02-03 10:33:05 -0500

    Firebase deployv2

--- public/app.js
+++ public/app.js
@@ -2,4 +2,4 @@ function suma (a, b) {
     return a + b;
 }
 
-document.getElementById("out").textContent = `La suma de 5 + 8 es ${suma(5, 8)}`;
+document.getElementById('out').textContent = `La suma de 5 + 8 es ${suma(5, 8)}`;
